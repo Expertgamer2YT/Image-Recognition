ce9e8e9d9fdd174346ee33fba5e09630fd84b281
Expertgamer2YT <91056870+Expertgamer2YT@users.noreply.github.com>
2023-11-22 18:33:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+https://teachablemachine.withgoogle.com/models/LXzrqZX2E/
+Webcam.set({
+    width:300,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+camera = document.getElementById("camera");
+Webcam.attach(camera);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/LXzrqZX2E/model.json", modelLoaded);
+function modelLoaded(){
+    console.log("Model is loaded");
+    console.log("ml5 version is",ml5.version);
+}
+function takesnapshot(){
+    Webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML="<img id='capture_image' src='"+data_uri+"'>";
+    });
+}
+
+
+
+
