5579106dbb54a0914b3534ad194e3385b1b52833
Expertgamer2YT <91056870+Expertgamer2YT@users.noreply.github.com>
2023-11-29 18:28:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,6 +13,22 @@ function modelLoaded(){
     console.log("Model is loaded");
     console.log("ml5 version is",ml5.version);
 }
+
+function check(){
+    img = document.getElementById("capture_image");
+        classifier.classify(img, gotresult);
+}
+
+function gotresult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML=results[0].label;
+        document.getElementById("object_accuracy").innerHTML=results[0].confidence.toFixed(2);
+    }
+}
 function takesnapshot(){
     Webcam.snap(function (data_uri){
         document.getElementById("result").innerHTML="<img id='capture_image' src='"+data_uri+"'>";
